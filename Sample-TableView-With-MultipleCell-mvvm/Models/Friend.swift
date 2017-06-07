dbed05ba2c08c511a544d192b11daf23baf374c0
takumi314 <isospin_symmetry@me.com>
2017-06-08 00:47:15 +0900

    make new model

---
+++ Sample-TableView-With-MultipleCell-mvvm/Models/Friend.swift
@@ -0,0 +1,9 @@
+//
+//  Friend.swift
+//  Sample-TableView-With-MultipleCell-mvvm
+//
+//  Created by NishiokaKohei on 2017/06/08.
+//  Copyright © 2017年 Kohey. All rights reserved.
+//
+
+import Foundation
