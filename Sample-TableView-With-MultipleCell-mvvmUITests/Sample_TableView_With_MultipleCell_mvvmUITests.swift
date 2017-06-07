2d5ce0cd9eaec32e03d377543a96487362ad0073
takumi314 <isospin_symmetry@me.com>
2017-06-08 00:13:02 +0900

    first init

---
+++ Sample-TableView-With-MultipleCell-mvvmUITests/Sample_TableView_With_MultipleCell_mvvmUITests.swift
@@ -0,0 +1,36 @@
+//
+//  Sample_TableView_With_MultipleCell_mvvmUITests.swift
+//  Sample-TableView-With-MultipleCell-mvvmUITests
+//
+//  Created by NishiokaKohei on 2017/06/08.
+//  Copyright © 2017年 Kohey. All rights reserved.
+//
+
+import XCTest
+
+class Sample_TableView_With_MultipleCell_mvvmUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
