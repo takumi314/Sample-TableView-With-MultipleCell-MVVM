a4baa811d6c9dfae54b75b8a6241a6bc1e18fb56
takumi314 <isospin_symmetry@me.com>
2017-06-11 21:56:56 +0900

    make new swift file

---
+++ Sample-TableView-With-MultipleCell-mvvm/ViewModels/ProfileViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  ProfileViewModel.swift
+//  Sample-TableView-With-MultipleCell-mvvm
+//
+//  Created by NishiokaKohei on 2017/06/11.
+//  Copyright © 2017年 Kohey. All rights reserved.
+//
+
+import Foundation
+
