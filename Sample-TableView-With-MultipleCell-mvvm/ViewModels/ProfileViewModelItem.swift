78311d10c97dc336f5d35de3c1bffc7285e9cf05
takumi314 <isospin_symmetry@me.com>
2017-06-11 22:04:14 +0900

    make new protocol file for ProfileVIewModelItems

---
+++ Sample-TableView-With-MultipleCell-mvvm/ViewModels/ProfileViewModelItem.swift
@@ -0,0 +1,9 @@
+//
+//  ProfileViewModelItem.swift
+//  Sample-TableView-With-MultipleCell-mvvm
+//
+//  Created by NishiokaKohei on 2017/06/11.
+//  Copyright © 2017年 Kohey. All rights reserved.
+//
+
+import Foundation
