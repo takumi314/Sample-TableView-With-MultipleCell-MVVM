fc5aa2e8a99d7547d5e099581025fbad5d222775
takumi314 <isospin_symmetry@me.com>
2017-06-12 01:00:50 +0900

    add new files to make classes inherited UITableViewCells

---
+++ Sample-TableView-With-MultipleCell-mvvm/Views/AboutCell.swift
@@ -0,0 +1,13 @@
+//
+//  AboutCell.swift
+//  Sample-TableView-With-MultipleCell-mvvm
+//
+//  Created by NishiokaKohei on 2017/06/12.
+//  Copyright © 2017年 Kohey. All rights reserved.
+//
+
+import UIKit
+
+class AboutCell: UITableViewCell {
+
+}
---
+++ Sample-TableView-With-MultipleCell-mvvm/Views/FriendCell.swift
@@ -0,0 +1,13 @@
+//
+//  FriendCell.swift
+//  Sample-TableView-With-MultipleCell-mvvm
+//
+//  Created by NishiokaKohei on 2017/06/12.
+//  Copyright © 2017年 Kohey. All rights reserved.
+//
+
+import UIKit
+
+class FriendCell: UITableViewCell {
+
+}
