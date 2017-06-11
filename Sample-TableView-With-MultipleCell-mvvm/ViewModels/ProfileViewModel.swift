8eb290dd545b1122d477db38c044dad6be4747da
takumi314 <isospin_symmetry@me.com>
2017-06-12 00:29:41 +0900

    set number of sections, number of rows in section

--- Sample-TableView-With-MultipleCell-mvvm/ViewModels/ProfileViewModel.swift
+++ Sample-TableView-With-MultipleCell-mvvm/ViewModels/ProfileViewModel.swift
@@ -63,11 +63,11 @@ class ProfileViewModel: NSObject {
 extension ProfileViewModel: UITableViewDataSource {
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return items[section].rowCount
     }
 
     public func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        return items.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
