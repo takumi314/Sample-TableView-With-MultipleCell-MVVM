cd69e572bda441fc0a89b87ec0cf11fb2339df7b
takumi314 <isospin_symmetry@me.com>
2017-06-13 01:38:02 +0900

    add new method to animate

--- Sample-TableView-With-MultipleCell-mvvm/Views/UIViewExtension.swift
+++ Sample-TableView-With-MultipleCell-mvvm/Views/UIViewExtension.swift
@@ -9,4 +9,16 @@
 import UIKit
 
 extension UIView {
+
+    func rotate(_ toValue: CGFloat, duration: TimeInterval = 0.2) {
+        let animation = CABasicAnimation(keyPath: "transform.rotation")
+
+        animation.toValue = toValue
+        animation.duration = duration
+        animation.isRemovedOnCompletion = false
+        animation.fillMode = kCAFillModeForwards
+
+        self.layer.add(animation, forKey: nil)
+    }
+
 }
