dc58590a7d2543f42bf66a13dd23426b92897ca8
takumi314 <isospin_symmetry@me.com>
2017-06-13 00:52:10 +0900

    make new nib file, swift file

---
+++ Sample-TableView-With-MultipleCell-mvvm/Views/ProfileHeaderView.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileHeaderView.swift
+//  Sample-TableView-With-MultipleCell-mvvm
+//
+//  Created by NishiokaKohei on 2017/06/13.
+//  Copyright © 2017年 Kohey. All rights reserved.
+//
+
+import UIKit
+
+protocol ProfileHeaderViewDelegate {
+    func toggleSection(header: ProfileHeaderView, section: Int)
+}
+
+
+class ProfileHeaderView: UITableViewHeaderFooterView {
+
+    
+
+}
+
