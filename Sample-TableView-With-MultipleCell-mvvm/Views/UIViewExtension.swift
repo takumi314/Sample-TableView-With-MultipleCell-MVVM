1c063ab78c0206f6158799481f0955180f518524
takumi314 <isospin_symmetry@me.com>
2017-06-13 01:33:24 +0900

    extend UIVIew to make animations

---
+++ Sample-TableView-With-MultipleCell-mvvm/Views/UIViewExtension.swift
@@ -0,0 +1,12 @@
+//
+//  UIViewExtension.swift
+//  Sample-TableView-With-MultipleCell-mvvm
+//
+//  Created by NishiokaKohei on 2017/06/13.
+//  Copyright © 2017年 Kohey. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+}
