0cef454ccbc2dafb35977c382e969ca8c1cadd5c
takumi314 <isospin_symmetry@me.com>
2017-06-13 02:04:43 +0900

    setup the properties and connection toTableView

--- Sample-TableView-With-MultipleCell-mvvm/ViewController.swift
+++ Sample-TableView-With-MultipleCell-mvvm/ViewController.swift
@@ -10,15 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - IBOutlets
+
+    @IBOutlet weak var profileTableView: UITableView!
+
+    // MARK: - Public Properties
+
+
+    // MARK: - Private properties
+
+    fileprivate let viewModel = ProfileViewModel()
+
+
+    // MARK: - Life cycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
+    // MARK: - Private methods
 
 
 }
